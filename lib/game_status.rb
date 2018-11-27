1f665c0f3c876129839374833ac8c8b87c85e45b
Fei Qi <feiqi9047@gmail.com>
2018-11-27 21:59:47 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -7,7 +7,7 @@ WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4
 
 def won?(board)
   WIN_COMBINATIONS.select do |combo|
-     combo.all? {|place| 
+     combo.all? {|index| board[index]=="X"
     position_taken?==true and 
   
   
