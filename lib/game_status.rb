bc13a15523f02d195d12c689fa9a5865cd447509
Fei Qi <feiqi9047@gmail.com>
2018-11-27 22:18:18 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -24,7 +24,7 @@ def draw?(board)
 end
 
 def over?(board)
-  if draw?(board)==true || won?(board)==true
+  if draw?(board)==true || full?(board)==true || won?(board)==true
     return true
   else
     return false
